c1dbe7fe61f2af6db6a34dd17066e1fe361dc206
于浩晨 <2028076336@qq.com>
2024-08-21 15:37:41 +0800

    list

--- day2/tood-list/src/App.jsx
+++ day2/tood-list/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component } from 'react';
+import  style from './App.module.scss';
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+   state = {
+    date: new Date().toString().slice(0,15)
+   }
+  
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+  render() {
+    console.log(this.state.date)
+    return (
+      <div className= {style.App}>
+        <div className={style.date}>
+            <span>{this.state.date}</span>
+        </div>
+        <div className={style.title}>
+            <span>6task</span>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
 
-export default App
+
+export default App;
